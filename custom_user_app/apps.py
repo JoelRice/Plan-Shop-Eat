1b7f411c79c7c954f8f1e29d9f7bd0f48d0dc7b5
Joel Rice <ricejoel17@gmail.com>
2021-10-05 12:55:01 -0400

    I added the file structure and installed pillow for images for the basic setup of the app.

---
+++ custom_user_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CustomUserAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'custom_user_app'
---
+++ meal_plan_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MealPlanAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meal_plan_app'
