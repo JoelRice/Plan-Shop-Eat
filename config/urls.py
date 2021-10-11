61f2538ce39ece5fcea7da5d580e04da49847108
Joel Rice <ricejoel17@gmail.com>
2021-10-11 15:30:43 -0400

    I created a user profile view, a user_profile.html template, a landing_page.html template and added a urls file to custom_user_app and uncommented the url already made in config. urls.py

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
     path('recipe_app/', include('recipe_app.urls')),
+    path('custom_user_app/', include('custom_user_app.urls')),
     # path('meal_plan_app/', include('meal_plan_app.urls')),
     # path('shopping_list_app/', include('shopping_list_app.urls')),
 ]
--- custom_user_app/views.py
+++ custom_user_app/views.py
@@ -1,1 +1,17 @@
-# from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from custom_user_app.models import CustomUser
+from recipe_app.models import Recipe
+
+@login_required
+def user_profile_view(request, user_id):
+    user_profile = CustomUser.objects.get(id=user_id)
+    user_favortied_recipes = user_profile.recipe_favorited_by.all()
+    return render(request, "user_profile.html",{"user_profile": user_profile, "user_favortied_recipes": user_favortied_recipes})
+    
+
+# def users_fav_recipe_view(request):
+#     favorites = request.user.favorited_by.all()
+#     user_favortied_recipes = list(favorites)
+#     return render(request, "user_profile.html",{"user_favortied_recipes": user_favortied_recipes})
