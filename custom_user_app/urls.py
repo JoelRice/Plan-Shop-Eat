13189bd3fe45a8dad34f8761711eb8d8104406ea
Joel Rice <ricejoel17@gmail.com>
2021-10-12 15:22:59 -0400

    I added a section for a users created recipes, meal plans, and shopping lists. I also fixed the user profile link for the navbar in base.html

--- custom_user_app/urls.py
+++ custom_user_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from custom_user_app.views import user_profile_view
-
 urlpatterns = [
-    path('profile/<int:user_id>/', user_profile_view, name='user_profile' ),
+    path('profile/<int:user_id>/', user_profile_view, name='user_profile' ), 
 ]
--- custom_user_app/views.py
+++ custom_user_app/views.py
@@ -2,10 +2,30 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from custom_user_app.models import CustomUser
+from meal_plan_app.models import MealPlan
 
 @login_required
+# recipe_custom_user is the related name for created_by
 def user_profile_view(request, user_id):
+    template_name = "user_profile.html"
     user_profile = CustomUser.objects.get(id=user_id)
     user_favortied_recipes = user_profile.recipe_favorited_by.all()
-    return render(request, "user_profile.html",{"user_profile": user_profile, "user_favortied_recipes": user_favortied_recipes, })
+    users_recipes = user_profile.recipe_custom_user.all()
+    return render(
+        request, 
+        template_name,
+        {
+        "user_profile": user_profile, 
+        "user_favortied_recipes": user_favortied_recipes,
+        "users_recipes": users_recipes
+        }
+        )
 
+
+# def users_meal_plan(request):
+#     template_name = "user_profile.html"
+#     plans = MealPlan.objects.all()
+#     return render(
+#         request, 
+#         template_name,
+#         {"plans": plans})
