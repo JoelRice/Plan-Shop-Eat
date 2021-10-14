575d3c0a35b304fc989342d99d48a7650142d676
Megan Dimond <medimond83@gmail.com>
2021-10-14 15:02:17 -0400

    added bottstrap and modified navbar

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/static/bootstrap/css',
+]
+
 # Default route for user login
 
 LOGIN_URL = '/authentication/login/'
