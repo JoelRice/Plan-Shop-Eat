e7f208b5911e860f95f0b3e44dd8c29f936ab456
Joel Rice <ricejoel17@gmail.com>
2021-10-15 16:41:16 -0400

    I fixed the nav bar margin, and added a logout button

--- authentication/urls.py
+++ authentication/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from authentication.views import login_view, signup_view
+from authentication.views import login_view, signup_view, logout_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name="signup"),
+    path('logout/', logout_view, name="logout"),
+    
 ]
